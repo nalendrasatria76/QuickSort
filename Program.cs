0200755f90a1a97c9a5660467b571553f95db8aa
TC1005 <curvafans383@gmail.com>
2022-10-19 16:57:55 +0700

    membuat numbers of elements in array

--- Program.cs
+++ Program.cs
@@ -1,10 +1,33 @@
 ﻿using System;
 
 namespace QuickSort
-{ 
+{
     class Program
     {
         //array of integers to hold values
         private int[] arr = new int[20];
         private int cmp_count = 0; // number of comparison
         private int mov_count = 0; // number of data movements
+
+        // numbers of elements in array
+        private int n;
+
+        void input()
+        {
+            while (true)
+            {
+                Console.Write("Enter the number of elements in the array :");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray can have maximum 20 elements \n");
+            }
+            Console.WriteLine("\n-------------------");
+            Console.WriteLine("Enter Array Elements");
+            Console.WriteLine("\n-------------------");
+
+        }
+    }
+}

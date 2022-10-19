48bf75ac598c84f45dee052314b72ebd1c1717f2
TC1005 <curvafans383@gmail.com>
2022-10-19 16:46:45 +0700

    membuat array of integers to hold values

--- Program.cs
+++ Program.cs
@@ -4,3 +4,7 @@ namespace QuickSort
 { 
     class Program
     {
+        //array of integers to hold values
+        private int[] arr = new int[20];
+        private int cmp_count = 0; // number of comparison
+        private int mov_count = 0; // number of data movements
